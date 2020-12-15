9eb09d822a13e57785a214c1d7cfcc9650a9d2fc
fffffggggg <duanxinyu44@163.com>
2020-12-15 09:28:20 +0800

    引入redis

---
+++ iapp/src/main/java/com/convenient/iapp/controller/CacheController.java
@@ -0,0 +1,28 @@
+package com.convenient.iapp.controller;
+
+import com.convenient.iapp.util.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/cache")
+@Slf4j
+public class CacheController {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @RequestMapping(path = "/set", method = RequestMethod.GET)
+    public boolean setValue(@RequestParam("key")String key, @RequestParam("value")String value){
+        return redisUtil.set(key, value);
+    }
+
+    @RequestMapping(path = "/get", method = RequestMethod.GET)
+    public Object getValue(@RequestParam("key")String key){
+        return redisUtil.get(key);
+    }
+}
